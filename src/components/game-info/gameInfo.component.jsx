4639847093873451f877441e7090f90fd02f60d4
Josh Ojuromi <45287264+Joshuromi@users.noreply.github.com>
2021-04-05 10:06:22 +0100

    created game play page

--- src/components/game-info/gameInfo.component.jsx
+++ src/components/game-info/gameInfo.component.jsx
@@ -1,27 +1,36 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 import { ReactComponent as Character } from "../../assets/greedy.svg";
 import "./gameInfo.style.scss";
 
-const GameInfo = () => (
-    <div className="game-info">
-        <div className="heading">
-            <Character />
-            <h1 className="heading-text">Greedy Hunter</h1>
-            <p className="details">
-                The aim is to eat all the food in record time<br />
+class GameInfo extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+    render() {
+        return (
+            <div className="game-info">
+                <div className="heading">
+                    <Character />
+                    <h1 className="heading-text">Greedy Hunter</h1>
+                    <p className="details">
+                        The aim is to eat all the food in record time<br />
                 Confiure your game grid below 👇🏼
             </p>
-        </div>
-        <div className="game-action">
-            <form >
-                <div className="input-div">
-                    <label>Game grid</label>
-                    <input type="number" name="grid" min="5" max="12" />
                 </div>
-                <button>Start game</button>
-            </form>
-        </div>
-    </div>
-);
+                <div className="game-action">
+                    <form >
+                        <div className="input-div">
+                            <label>Game grid</label>
+                            <input type="number" name="grid" min="5" max="12" />
+                        </div>
+                        <button>Start game</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
 
-export default GameInfo;
+export default withRouter(GameInfo);
--- src/pages/game-play/gamePlay.js
+++ src/pages/game-play/gamePlay.js
@@ -1,22 +1,9 @@
-
-import { ReactComponent as Background } from "../../assets/background.png";
-import Grids from "../../components/grid-cards/grid-cards.component";
-import { ReactComponent as Life } from "../../assets/life.svg";
 import "./gamePlay.style.scss";
 
 const GamePlay = () => (
     <div className="game-play">
-        <div className="board">
-            <div className="top">
-                <p>Grid 10 x 10</p>
-                <Life />
-                <p>Time Spent: 00:48s</p>
-            </div>
-            <Grids grid={10} />
-            <div className="bottom">
-                <p>Maximum moves: 100</p>
-                <p>Total moves: 12</p>
-            </div>
+        <div className="game-board">
+
         </div>
     </div>
 )
